96e8a58e34c11493fb7ec7856a95f885fc151dec
uuumrr <uuumrr@noreply.gitcode.com>
2025-07-17 21:45:36 +0800

    整合并重构所有功能，但OCR申请权限时存在闪退问题

--- Agent/OpenCV/build/intermediates/cxx/Debug/6z562g10/logs/armeabi-v7a/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\HWH\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "E:\\Expense-Buddy\\Agent\\OpenCV\\.cxx\\Debug\\6z562g10\\armeabi-v7a" ^
-  opencv_jni_shared
--- Agent/OpenCV/build/intermediates/cxx/Debug/6z562g10/logs/x86_64/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\HWH\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "E:\\Expense-Buddy\\Agent\\OpenCV\\.cxx\\Debug\\6z562g10\\x86_64" ^
-  opencv_jni_shared
